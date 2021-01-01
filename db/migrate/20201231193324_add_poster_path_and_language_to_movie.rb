d40f01c49babf9791f7ee608f8746b8ece376769
Amr <amr.ayoub@coformatique.com>
2021-01-01 05:36:53 +0200

    Added Elasticsearch for news content, aslo Fixed some model and migrations conflicts

--- db/migrate/20201231193324_add_poster_path_and_language_to_movie.rb
+++
@@ -1,6 +0,0 @@
-class AddPosterPathAndLanguageToMovie < ActiveRecord::Migration[6.1]
-  def change
-    add_column :movies, :poster_path, :string
-    add_column :movies, :language, :string
-  end
-end
