44a74791c8caa1fc7d0f23072db1b11d76b75167
Amr <amr.ayoub@coformatique.com>
2021-01-01 04:11:25 +0200

    Added Gender Enum To Table Celecrities

--- db/migrate/20201228232941_create_celebrities.rb
+++ db/migrate/20201228232941_create_celebrities.rb
@@ -4,7 +4,7 @@ class CreateCelebrities < ActiveRecord::Migration[6.1]
       t.text :display_name, :first_name, :last_name, null: false
       t.date :birthdate, null: false
       t.date :deathdate
-      t.text :gender, :nationality, :biography, null: false
+      t.text :nationality, :biography, null: false
       t.text :place_of_birth, null: false
 
       t.timestamps
